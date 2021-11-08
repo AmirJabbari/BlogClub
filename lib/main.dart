ca6a223315be3b0c74ceb31ff37e9643039d440f
AmirJabbarii <am.jabbarii@gmail.com>
2021-11-08 15:20:10 +0330

    Add Appbar Add story list

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:blogclub/data.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,26 +6,132 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const defaultFontFamily = 'Avenir';
+
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final primaryTextColor = Color(0xff0D253C);
+    final secondaryTextColor = Color(0xff2D4379);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.blue,
+          textTheme: TextTheme(
+              subtitle1: TextStyle(
+                  fontFamily: defaultFontFamily,
+                  color: secondaryTextColor,
+                  fontSize: 14),
+              headline6: TextStyle(
+                  fontFamily: defaultFontFamily,
+                  fontWeight: FontWeight.bold,
+                  color: primaryTextColor),
+              bodyText2: TextStyle(
+                  fontFamily: defaultFontFamily,
+                  color: secondaryTextColor,
+                  fontSize: 12))),
+      home: const HomeScreen(),
     );
   }
 }
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    final stories = AppDatabase.stories;
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 16, 32, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Hi, Jonathan!',
+                      style: themeData.textTheme.subtitle1,
+                    ),
+                    Image.asset(
+                      'assets/img/icons/notification.png',
+                      width: 24,
+                      height: 24,
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 0, 0, 24),
+                child: Text(
+                  "Explore today’s",
+                  style: themeData.textTheme.headline6,
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 90,
+                child: ListView.builder(
+                    itemCount: stories.length,
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+                    itemBuilder: (context, index) {
+                      final story = stories[index];
+                      return Container(
+                        margin: const EdgeInsets.fromLTRB(4, 0, 4, 0),
+                        child: Column(
+                          children: [
+                            Stack(
+                              children: [
+                                Container(
+                                  width: 68,
+                                  height: 68,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(24),
+                                      gradient: const LinearGradient(
+                                          begin: Alignment.topLeft,
+                                          colors: [
+                                            Color(0xff376AED),
+                                            Color(0xff49B0E2),
+                                            Color(0xff9CECFB)
+                                          ])),
+                                  child: Container(
+                                    margin: const EdgeInsets.all(3),
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    padding: const EdgeInsets.all(4),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(16),
+                                      child: Image.asset(
+                                          'assets/img/stories/${story.imageFileName}'),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                    right: 0,
+                                    child: Image.asset('assets/img/icons/${story.iconFileName}',width: 24,height: 24,))
+                              ],
+                            ),
+                            const SizedBox(height: 8),
+                            Text(story.name)
+                          ],
+                        ),
+                      );
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
