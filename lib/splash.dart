0aa515c5951b2ebc231248d84a13b676cf94ebba
AmirJabbarii <am.jabbarii@gmail.com>
2021-11-29 11:13:15 +0330

    Add Splash screen

---
+++ lib/splash.dart
@@ -0,0 +1,36 @@
+import 'package:blogclub/gen/assets.gen.dart';
+import 'package:blogclub/home.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 2)).then((value) {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => const HomeScreen()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+              child: Assets.img.background.splash.image(fit: BoxFit.cover)),
+          Center(
+            child: Assets.img.icons.logo.svg(width: 100),
+          )
+        ],
+      ),
+    );
+  }
+}
